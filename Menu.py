8d469f8d30d0da96c0c97875c9006d317fab7707
alexZ7000 <78627928+alexZ7000@users.noreply.github.com>
2023-06-13 11:43:49 -0300

    Menu.py

--- Menu.py
+++ Menu.py
@@ -15,31 +15,36 @@ root.geometry(f'{x}x{y}')  # Dimensão da tela
 root.minsize(1910, 1070)  # Resolução mínima para redimencionalizar
 root.maxsize(1920, 1080)  # Forçar resolução Full HD (1920x1080) do DevQuiz
 root.attributes("-fullscreen", 1)  # Colocar o DevQuiz em tela cheia
+claro = "#ccccff"
+escuro = "#1D1D66"
+
 
 class Menu:
     """Classe que irá definir todos os itens que todos os "Menu" vão usar"""
+
+    c = 0
+
+    def change_theme(self):
+        """ Função para mudar de tema "Claro" para tema "Escuro"
+        :param self: Menu
+        :returns: Não retorna nada"""
+        self.claro = "#ccccff"
+        self.escuro = "#1D1D66"
+        if self.c % 2 == 0:
+            self.frame.config(bg="#ccccff")
+            return self.c
+        else:
+            self.frame.config(bg="#1D1D66")
+            return "#1D1D66"
+
+
 
     def __init__(self):
         """Criação da minha tela"""
         self.theme_txt = None
         self.dev_system = None
-        self.frame = Frame(root, bg="#ccccff")
+        self.frame = Frame(root, bg=str(claro if self.c % 2 == 0 else escuro))
         self._build_screen()
-
-
-    # c = 0
-    #
-    # def change_theme(self):
-    #     """ Função para mudar de tema "Claro" para tema "Escuro"
-    #      :param self: Menu
-    #      :returns: Não retorna nada"""
-    #     self.c = self.c + 1
-    #     if self.c % 2 == 0:
-    #         self.theme_txt.set("Tema Claro")
-    #         self.frame.config(bg="#ccccff")
-    #     else:
-    #         self.theme_txt.set("Tema Escuro")
-    #         self.frame.config(bg="#1D1D66")
 
     def set_dev_system(self, dev_system):
         """ Função para colocar os objetos referenciados no "DevSystem" em todas as Classes que herdarem de "Menu".
